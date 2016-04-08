b18b946511c5b432be7cfc1513b931c20ba1c6b1
Walther <veeti.haapsamo@gmail.com>
2016-04-08 19:49:26 +0300

    Add first test

---
+++ test/server.js
@@ -0,0 +1,22 @@
+var server = require('../server.js');
+var assert = require('assert');
+var http   = require('http');
+
+describe('server', function () {
+  before(function () {
+    server.listen(5000);
+  });
+
+  after(function () {
+    server.close();
+  });
+});
+
+describe('http get /', function () {
+  it('should return 200', function (done) {
+    http.get('http://localhost:5000', function (res) {
+      assert.equal(200, res.statusCode);
+      done();
+    });
+  });
+});
