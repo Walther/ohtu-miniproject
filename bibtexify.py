724a42855531368afb5d865ac7713e47345063ac
Matti Remes <matti.remes@eficode.com>
2016-04-21 00:11:13 +0300

    refactored code and added a comment

--- bibtexify.py
+++ bibtexify.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == '__main__':
     filenames = sys.argv[1]
     filenames = filenames.split(",")
-    filenames.pop(0)
+    filenames.pop(0) # to remove static .gitignore from the list TODO reformat
     output = open('./bibtex-list.txt', 'w')
     for filename in filenames:
         inputFile = open('./data/' + filename,'r')
@@ -18,7 +18,7 @@ if __name__ == '__main__':
         output.write(item)
         inputFile.close()
     output.close()
-    output = open('./bibtex-list', 'r')
+    output = open('./bibtex-list.txt', 'r')
     contents = output.read()
     print(contents)
     sys.stdout.flush()
