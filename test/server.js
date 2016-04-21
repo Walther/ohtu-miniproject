69384ac594ac3ccdd2804ef2bfc3ae7cd492deaf
Walther <veeti.haapsamo@gmail.com>
2016-04-21 13:12:52 +0300

    Add more user stories / tests

--- test/server.js
+++ test/server.js
@@ -1,6 +1,11 @@
-var server = require('../server.js');
-var assert = require('assert');
-var http   = require('http');
+var server  = require('../server.js');
+var assert  = require('assert');
+var http    = require('http');
+var request = require("request");
+var should  = require("should");
+var fs      = require("fs");
+var DATADIR = __dirname + "/../data/";
+
 
 describe('server', function () {
   before(function () {
@@ -14,8 +19,82 @@ describe('server', function () {
 
 describe('when user goes to front page', function () {
   it('should return 200', function (done) {
-    http.get('http://localhost:5000', function (res) {
-      assert.equal(200, res.statusCode);
+      request.get('http://localhost:5000', function (err, res, body) {
+      res.statusCode.should.equal(200);
+      done();
+    });
+  });
+});
+
+describe('when user goes to front page', function () {
+  it('should contain citation lists', function (done) {
+    request.get('http://localhost:5000', function (err, res, body) {
+      body.should.containEql("List of articles");
+      body.should.containEql("List of books");
+      body.should.containEql("List of inproceedings");
+      done();
+    });
+  });
+});
+
+describe('when user submits article', function () {
+  it('article is saved', function (done) {
+    request.post('http://localhost:5000', {
+      form:{
+        Format:'article',
+        Author:'TestAuthor',
+        Title:'TestTitle',
+        Journal:'TestJournal',
+        Volume:'1',
+        Year:'2016',
+        Pages:'12'
+      }}, function(err,res,body){
+        res.should.equal(200);
+      });
+    done();
+  });
+});
+
+describe('when user submits book', function () {
+  it('book is saved', function (done) {
+    request.post('http://localhost:5000', {
+      form:{
+        Format:'book',
+        Author:'TestAuthor',
+        Title:'TestTitle',
+        Publisher:'TestPublisher',
+        Year:'2016'
+      }}, function(err,res,body){
+        res.should.equal(200);
+      });
+    done();
+  });
+});
+
+describe('when user submits inproceedings', function () {
+  it('inproceedings is saved', function (done) {
+    request.post('http://localhost:5000', {
+      form:{
+        Format:'inproceedings',
+        Author:'TestAuthor',
+        Title:'TestTitle',
+        Booktitle:'TestBooktitle',
+        Year:'2016'
+      }}, function(err,res,body){
+        res.should.equal(200);
+      });
+    done();
+  });
+});
+
+
+describe('when user requests bibtex file', function () {
+  it('bibtex file is provided', function (done) {
+    request.get('http://localhost:5000/references.bib', function (err, res, body) {
+      res.statusCode.should.equal(200);
+      body.should.containEql("article");
+      body.should.containEql("book");
+      body.should.containEql("inproceedings");
       done();
     });
   });
