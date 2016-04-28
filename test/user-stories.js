8a2f471ba6b8033f632d952d40c1c828f388a77d
Otto Porkka <otto.porkka@helsinki.fi>
2016-04-28 15:47:21 +0300

    Tests for user stories added

--- test/user-stories.js
+++ test/user-stories.js
@@ -57,6 +57,7 @@ describe('when user requests bibtex file', function () {
   });
 });
 
+//Tests for article
 describe('when user submits article', function () {
   it('server responds OK', function () {
     request.post('http://localhost:5000/submit', {
@@ -110,6 +111,7 @@ describe('when user submits article', function () {
   });
 });
 
+//Tests for book
 describe('when user submits book', function () {
   it('book is saved', function () {
     request.post('http://localhost:5000/submit', {
@@ -159,6 +161,7 @@ describe('when user submits book', function () {
   });
 });
 
+//Tests for inproceedings
 describe('when user submits inproceedings', function () {
   it('inproceedings is saved', function () {
     request.post('http://localhost:5000/submit', {
@@ -207,3 +210,262 @@ describe('when user submits inproceedings', function () {
     });
   });
 });
+
+//Tests for new forms. Added by Otto.
+//Tests for incollection
+describe('when user submits incollection', function () {
+  it('incollection is saved', function () {
+    request.post('http://localhost:5000/submit', {
+      form:{
+        Format:'incollection',
+        Author:'TestAuthor',
+        Title:'TestTitle',
+        Booktitle:'TestBooktitle',
+	Publisher:'TestPublisher',
+        Year:'2016'
+      }}, function(err,res,body){
+        res.should.equal(200);
+      });
+  });
+  it('incollection is saved into a file', function () {
+    var filelist = [];
+    fs.readdir(DATADIR, function (err, files) {
+      if (err) {
+        return console.log(err);
+      }
+      files.forEach(function (filename) {
+        if(! /^\..*/.test(filename)) {
+          filelist.push(filename);
+        }
+      });
+      filelist.length.should.equal(1);
+    });
+  });
+  it('incollection is found in listing', function() {
+    request.get('http://localhost:5000/list', function(err, res, body) {
+      body.should.containEql("incollection");
+      body.should.containEql("author");
+      body.should.containEql("title");
+      body.should.containEql("booktitle");
+      body.should.containEql("publisher");
+      body.should.containEql("year");
+    });
+  });
+  it('bibtex will have incollection', function() {
+    request.get('http://localhost:5000/references.bib', function (err, res, body) {
+      res.statusCode.should.equal(200);
+      body.should.containEql("id");
+      body.should.containEql("incollection");
+      body.should.containEql("author");
+      body.should.containEql("title");
+      body.should.cońtainEql("booktitle");
+      body.should.containEql("publisher");
+      body.should.containEql("year");
+    });
+  });
+});
+
+//Tests for inbook
+describe('when user submits inbook', function () {
+  it('server responds OK', function () {
+    request.post('http://localhost:5000/submit', {
+      form:{
+        Format:'inbook',
+        Author:'TestAuthor',
+        Title:'TestTitle',
+        Publisher:'TestPublisher',
+        Year:'2016',
+        Pages:'12'
+      }}, function(err,res,body){
+        res.should.equal(200);
+      });
+  });
+  it('inbook is saved into a file', function () {
+    var filelist = [];
+    fs.readdir(DATADIR, function (err, files) {
+      if (err) {
+        return console.log(err);
+      }
+      files.forEach(function (filename) {
+        if(! /^\..*/.test(filename)) {
+          filelist.push(filename);
+        }
+      });
+      filelist.length.should.equal(1);
+    });
+  });
+  it('inbook is found in listing', function() {
+    request.get('http://localhost:5000/list', function(err, res, body) {
+      body.should.containEql("inbook");
+      body.should.containEql("author");
+      body.should.containEql("title");
+      body.should.containEql("publisher");
+      body.should.containEql("year");
+      body.should.containEql("pages");
+    });
+  });
+  it('bibtex will have article', function() {
+    request.get('http://localhost:5000/references.bib', function (err, res, body) {
+      res.statusCode.should.equal(200);
+      body.should.containEql("id");
+      body.should.containEql("inbook");
+      body.should.containEql("author");
+      body.should.containEql("title");
+      body.should.containEql("publisher");
+      body.should.containEql("year");
+      body.should.containEql("pages");
+    });
+  });
+});
+
+//Tests for masterthesis
+describe('when user submits masterthesis', function () {
+  it('server responds OK', function () {
+    request.post('http://localhost:5000/submit', {
+      form:{
+        Format:'masterthesis',
+        Author:'TestAuthor',
+        Title:'TestTitle',
+        School:'TestSchool',
+        Year:'2016'
+      }}, function(err,res,body){
+        res.should.equal(200);
+      });
+  });
+  it('masterthesis is saved into a file', function () {
+    var filelist = [];
+    fs.readdir(DATADIR, function (err, files) {
+      if (err) {
+        return console.log(err);
+      }
+      files.forEach(function (filename) {
+        if(! /^\..*/.test(filename)) {
+          filelist.push(filename);
+        }
+      });
+      filelist.length.should.equal(1);
+    });
+  });
+  it('masterthesis is found in listing', function() {
+    request.get('http://localhost:5000/list', function(err, res, body) {
+      body.should.containEql("masterthesis");
+      body.should.containEql("author");
+      body.should.containEql("title");
+      body.should.containEql("school");
+      body.should.containEql("year");
+    });
+  });
+  it('bibtex will have masterthesis', function() {
+    request.get('http://localhost:5000/references.bib', function (err, res, body) {
+      res.statusCode.should.equal(200);
+      body.should.containEql("id");
+      body.should.containEql("masterthesis");
+      body.should.containEql("author");
+      body.should.containEql("title");
+      body.should.containEql("school");
+      body.should.containEql("year");
+    });
+  });
+});
+
+//Tests for phdthesis
+describe('when user submits phdthesis', function () {
+  it('server responds OK', function () {
+    request.post('http://localhost:5000/submit', {
+      form:{
+        Format:'phdthesis',
+        Author:'TestAuthor',
+        Title:'TestTitle',
+        School:'TestSchool',
+        Year:'2016'
+      }}, function(err,res,body){
+        res.should.equal(200);
+      });
+  });
+  it('phdthesis is saved into a file', function () {
+    var filelist = [];
+    fs.readdir(DATADIR, function (err, files) {
+      if (err) {
+        return console.log(err);
+      }
+      files.forEach(function (filename) {
+        if(! /^\..*/.test(filename)) {
+          filelist.push(filename);
+        }
+      });
+      filelist.length.should.equal(1);
+    });
+  });
+  it('phdthesis is found in listing', function() {
+    request.get('http://localhost:5000/list', function(err, res, body) {
+      body.should.containEql("phdthesis");
+      body.should.containEql("author");
+      body.should.containEql("title");
+      body.should.containEql("school");
+      body.should.containEql("year");
+    });
+  });
+  it('bibtex will have phdthesis', function() {
+    request.get('http://localhost:5000/references.bib', function (err, res, body) {
+      res.statusCode.should.equal(200);
+      body.should.containEql("id");
+      body.should.containEql("phdthesis");
+      body.should.containEql("author");
+      body.should.containEql("title");
+      body.should.containEql("school");
+      body.should.containEql("year");
+    });
+  });
+});
+
+
+//Tests for techreport
+describe('when user submits techreport', function () {
+  it('server responds OK', function () {
+    request.post('http://localhost:5000/submit', {
+      form:{
+        Format:'techreport',
+        Author:'TestAuthor',
+        Title:'TestTitle',
+        Institution:'TestInstitution',
+        Year:'2016'
+      }}, function(err,res,body){
+        res.should.equal(200);
+      });
+  });
+  it('techreport is saved into a file', function () {
+    var filelist = [];
+    fs.readdir(DATADIR, function (err, files) {
+      if (err) {
+        return console.log(err);
+      }
+      files.forEach(function (filename) {
+        if(! /^\..*/.test(filename)) {
+          filelist.push(filename);
+        }
+      });
+      filelist.length.should.equal(1);
+    });
+  });
+  it('techreport is found in listing', function() {
+    request.get('http://localhost:5000/list', function(err, res, body) {
+      body.should.containEql("techreport");
+      body.should.containEql("author");
+      body.should.containEql("title");
+      body.should.containEql("institution");
+      body.should.containEql("year");
+    });
+  });
+  it('bibtex will have techreport', function() {
+    request.get('http://localhost:5000/references.bib', function (err, res, body) {
+      res.statusCode.should.equal(200);
+      body.should.containEql("id");
+      body.should.containEql("techreport");
+      body.should.containEql("author");
+      body.should.containEql("title");
+      body.should.containEql("institution");
+      body.should.containEql("year");
+    });
+  });
+});
+
